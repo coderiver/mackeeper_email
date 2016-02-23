6928accb37548d25547e27bf8f160a7fab6ffdd2
akella <akella.a@gmail.com>
2016-02-23 20:11:58 +0200

    final

--- gulp/tasks/mail.js
+++ gulp/tasks/mail.js
@@ -5,9 +5,9 @@ sendmail = require('gulp-mailgun');
 gulp.task('mail', function () {
   gulp.src( 'build/index.html') // Modify this to select the HTML file(s)
   .pipe(sendmail({
-    key: 'key-a12658a24799e13a7f50e297ad04f2c9', // Enter your Mailgun API key here
+    key: 'key-a12658a24799e13a7f50e297ad04f2c9', 
     sender: 'from@test.com',
-    recipient: 'akella.a@gmail.com',
+    recipient: 'flintyara@yahoo.com',
     subject: 'This is a test email'
   }));
 });
